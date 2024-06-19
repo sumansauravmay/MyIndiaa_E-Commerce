a2ad6c199d98b347e9ef0f0b2e57752516459c4f
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-20 01:16:09 +0530

    product management

--- routes/product.route.js
+++ routes/product.route.js
@@ -14,18 +14,41 @@ productRouter.get("/", async (req, res) => {
   }
 });
 
-productRouter.post("/add", (req, res)=>{
-    const data=req.body;
-    try{
-const product=new ProductModel(data);
- product.save();
-res.send("added product")
-    }catch(err){
-        console.log(err);
-    }
-})
+productRouter.post("/add", (req, res) => {
+  const data = req.body;
+  try {
+    const product = new ProductModel(data);
+    product.save();
+    res
+      .status(201)
+      .send({ msg: "Product added successfully", product: product });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "Failed to add product" });
+  }
+});
 
+productRouter.patch("/update/:id", async (req, res) => {
+  const payload = req.body;
+  const ID = req.params.id;
 
-module.exports={productRouter};
+  try {
+    await ProductModel.findByIdAndUpdate({ _id: ID }, payload);
+    res.send("Updated the product details!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "Failed to update product" });
+  }
+});
 
+productRouter.delete("/delete/:id", async (req, res) => {
+  const ID = req.params.id;
+  try {
+    await ProductModel.findByIdAndDelete({ _id: ID });
+    res.send("Deleted the product!");
+  } catch (err) {
+    res.status(500).send({ msg: "Failed to delete product" });
+  }
+});
 
+module.exports = { productRouter };
