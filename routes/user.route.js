6d6b8ec39fc2c01e0dc008a180e608a1307c6045
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-19 16:29:55 +0530

    register and login bcrypt

--- routes/user.route.js
+++ routes/user.route.js
@@ -11,49 +11,29 @@ userRouter.post("/register", async (req, res) => {
   const payload = req.body;
   try {
     const salt = await bcrypt.genSalt(10);
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(payload.password, salt);
-
-    // Replace the plain text password with the hashed password
-    payload.password = hashedPassword;
+    const hashedPass = await bcrypt.hash(payload.password, salt);
+    payload.password = hashedPass;
 
     const user = new UserModel(payload);
     user.save();
-    res.send({ msg: "User registered successfully", user });
+    res.send({ msg: "User registered successfully!", user });
   } catch (err) {
     console.log(err);
-    res.status(500).send({ msg: "Internal Server Error" });
+    res.status(500).send({ msg: "Error" });
   }
 });
 
 userRouter.post("/login", async (req, res) => {
-  //   const { email, password } = req.body;
-  //   try {
-  //     const user = await UserModel.find({ email });
-  //     await bcrypt.compare(password, user.password);
-  //     var token = jwt.sign({ foo: "bar" }, "sjfd");
-
-  //     if (token > 0) {
-  //       res.send({ msg: "Login Successful", token: token });
-  //       console.log(user);
-  //     } else {
-  //       res.send("Wrong Credential!");
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-
   const { email, password } = req.body;
   try {
     const user = await UserModel.findOne({ email });
     if (!user) {
-      return res.status(401).send({ msg: "Wrong Credentials!" });
+      return res.status(401).send({ msg: "Wrong Email!" });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).send({ msg2: "Wrong Credentials!" });
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res.status(401).send({ msg2: "Wrong Password!" });
     }
 
     const token = jwt.sign(
